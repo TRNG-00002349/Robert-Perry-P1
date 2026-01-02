f37c95372cc017d5e56e989e209f75cbb4449eef
Robert <tres.perry@gmail.com>
2026-01-02 15:59:11 -0600

    init user controller and exceptions

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,8 +1,35 @@
 package com.revature.controllers;
 
+import com.revature.services.UserService;
+
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+
+import com.revature.entities.User;
+import com.revature.exceptions.UniquenessViolationException;
 /*
     User controller, handles all HTTP Requests
 */
 public class UserController {
     
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    //Handle all user requests and user related errors
+
+    public void create(Context ctx) throws UniquenessViolationException{
+        User newUser = ctx.bodyAsClass(User.class);
+        User finalUser = userService.create(newUser);
+
+        ctx.status(HttpStatus.CREATED);
+        ctx.json(finalUser);
+    }
+
+    public void handleUniqenessViolationException(UniquenessViolationException e, Context ctx){
+        ctx.status(HttpStatus.CONFLICT);
+        ctx.result("The username or email is already associated with another account.");
+    }
+
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,9 +1,31 @@
 package com.revature.services;
 
+import java.sql.SQLException;
+
+import com.revature.daos.UserDao;
+import com.revature.entities.User;
 /*
     User service, business logic
 */
+import com.revature.exceptions.UniquenessViolationException;
 
 public class UserService {
     
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public User create(User user) throws UniquenessViolationException{
+        try{
+           return userDao.saveUser(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new User();
+    }
+
+
 }
